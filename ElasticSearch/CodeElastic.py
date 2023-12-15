a538ed546ed6108381ef4f8e552743c0f72af0d9
JPLS2000 <joaopedro.jplsouza@gmail.com>
2023-12-14 21:20:19 -0300

    feat finally ajustes

--- ElasticSearch/CodeElastic.py
+++ ElasticSearch/CodeElastic.py
@@ -1,14 +1,27 @@
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 import timeit
 from dotenv import load_dotenv
 import os
+import matplotlib.pyplot as plt
 
 
-def VerificaDiretorioIndex(index_diretorio, es):
-    if not es.indices.exists(index=index_diretorio):
-        es.indices.create(index=index_diretorio)
-        print("Indexando...")
-        lerDiretorioIndex(index_diretorio, arquivo)
+def documentosRelevantes(nome, i):
+    val = []
+    with open(nome, "r") as arquivo:
+        for linha in arquivo:
+            if int(linha.split()[0]) == (i+1):
+                val.append(''.join(linha.split()[1]))
+
+    return val
+
+
+def getPrecision(Ra, R):
+    return Ra/R
+
+
+def getRecall(Ra, A):
+    return Ra/A
 
 
 def limparQuery(string):
@@ -40,7 +53,9 @@ def lerColecao(path):
     return f.split(".I")[1:]
 
 
-def lerDiretorioIndex(index_name, arquivo):
+def lerDiretorioIndex(index_name, arquivo, es):
+    docs = []
+    tempoIndexacao = timeit.default_timer()
     for x, linha in enumerate(lerColecao(arquivo)):
         content = limparLixo(linha)
         index = x
@@ -48,13 +63,33 @@ def lerDiretorioIndex(index_name, arquivo):
         title = ''.join(''.join(content.split(" .T ")[1:]).split(" .A ")[:1])
         content = ''.join(''.join(content.split(" .T ")[1:]).split(" .A ")[1:])
         doc = {
-            'index': index,
-            'identificador': i,
-            'title': title,
-            'content': content
+            '_index': index_name,
+            '_id': index,
+            '_source': {
+                'index': i,
+                'title': title,
+                'content': content
+            }
         }
-        es.index(index=index_name, id=index, body=doc)
-        print(doc)
+        docs.append(doc)
+
+    tempoIndexacao = timeit.default_timer() - tempoIndexacao
+    success, failled = bulk(es, docs)
+    return tempoIndexacao
+
+
+def VerificaDiretorioIndex(index_diretorio, arquivo, es):
+    if not es.indices.exists(index=index_diretorio):
+        es.indices.create(index=index_diretorio)
+    tempoIndexacao = lerDiretorioIndex(index_diretorio, arquivo, es)
+    return tempoIndexacao
+
+
+def plotarGRaficos(nome, x, y, namex, namey):
+    plt.plot(x, y, color='red')
+    plt.xlabel(namex)
+    plt.ylabel(namey)
+    plt.show()
 
 
 load_dotenv()
@@ -64,61 +99,94 @@ es = Elasticsearch(
     basic_auth=(os.environ.get('ELASTIC_NAME'), os.environ.get('ELASTIC_PASSWORD')))
 index_diretorio = "my_index"
 arquivo = "DataSets/cran.all.1400"
-VerificaDiretorioIndex(index_diretorio, es)
-tipo_teste = int(
-    input("Digite o tipo de teste:\n1.Teste Automatico\n2.Teste Manual\n"))
-if (tipo_teste == 1):
-    arquivo_query = "DataSets/cran.qry"
-    arquivoResposta = open("Respostas/elasticRespostasCran.txt", "a")
-    arquivoResposta.truncate(0)
-    for x, linha in enumerate(lerColecao(arquivo_query)):
-        busca = limparQuery(linha)
-        i = ''.join(busca.split(" .W ")[:1])
-        busca = ''.join(busca.split(" .W ")[1:])
-        #buscarOR = busca.replace(" ", " OR ")
-        results = es.search(index=index_diretorio,
-                            body={
-                                "query": {
-                                    "match": {
-                                        "content": busca
-                                    }
+tempoIndexacao = VerificaDiretorioIndex(index_diretorio, arquivo, es)
+# Caso queira Habiilitar a busca Manual descomente esse codigo e ajuste a identação:
+# tipo_teste = int(
+#     input("Digite o tipo de teste:\n1.Teste Automatico\n2.Teste Manual\n"))
+# if (tipo_teste == 1):
+arquivo_query = "DataSets/cran.qry"
+arquivoResposta = open("Respostas/elasticRespostasCran.txt", "a")
+arquivoResposta.truncate(0)
+precision = []
+recall = []
+tempoBusca = []
+perguntas = []
+docsRelev = []
+docs = []
+for x, linha in enumerate(lerColecao(arquivo_query)):
+    doc = []
+    perguntas.append(x+1)
+    busca = limparQuery(linha)
+    i = ''.join(busca.split(" .W ")[:1])
+    busca = ''.join(busca.split(" .W ")[1:])
+    temp = timeit.default_timer()
+    results = es.search(index=index_diretorio,
+                        body={
+                            "query": {
+                                "multi_match": {
+                                    "query": busca,
+                                    "fields": ["title", "content"]
                                 }
                             }
-                            )
-        for hit in results['hits']['hits']:
-            arquivoResposta.write(
-                f"Linha:{x+1}-IDPergunta:{i}-IDDocumento:{hit['_source']['identificador']}-Score:{hit['_score']}\n")
-            arquivoResposta.write(f"Pergunta:{busca}\n")
-            arquivoResposta.write(f"Titulo:{hit['_source']['title']}\n")
-            arquivoResposta.write(
-                f"--------------------------------------------\n")
-    arquivoResposta.close()
-elif (tipo_teste == 2):
-    tipoBusca = int(input(
-        "Digite o tipo de busca (Linha ou Texto):\n1.Numero Estudo\n2.Conteudo\n3.Titulo\n "))
-    if (tipoBusca == 2):
-        busca = input("Digite o conteudo de busca: ")
-        results = es.search(index=index_diretorio, body={
-            "query": {"match": {"content": busca}}})
-        flag = True
+                        }
+                        )
+    temp = timeit.default_timer()-temp
+    for hit in results['hits']['hits']:
+        doc.append(hit['_source']['index'])
+        arquivoResposta.write(
+            f"Linha:{x+1}-IDPergunta:{i}-IDDocumento:{hit['_source']['index']}-Score:{hit['_score']}\n")
+        arquivoResposta.write(f"Pergunta:{busca}\n")
+        arquivoResposta.write(f"Titulo:{hit['_source']['title']}\n")
+        arquivoResposta.write(
+            f"--------------------------------------------\n")
+    docReelev = documentosRelevantes("DataSets/cranqrel", x)
+    docsRelev.append(docReelev)
+    docs.append(doc)
+    tempoBusca.append(temp)
+for k in range(10):
+    p = []
+    r = []
+    for i, linha in enumerate(docs):
 
-    elif (tipoBusca == 1):
-        busca = input("Digite o numero da linha: ")
-        results = results = results = es.search(index=index_diretorio, body={
-            "query": {"match": {"identificador": busca}}})
-        flag = True
-    elif (tipoBusca == 3):
-        busca = input("Digite o parte do Titulo do estudo: ")
-        results = results = es.search(index=index_diretorio, body={
-            "query": {"match": {"title": busca}}})
-        flag = True
-    else:
-        flag = False
-        print("Opção invalida")
-    if (flag == True):
-        for x in results['hits']['hits']:
-            print(
-                f"ID:{x['_souce']['indentificador']}\nTitle: {x['_source']['title']}\nCorpo: {x['_source']['content']}\n")
-            print('--------------------------------------------\n')
+        releRecupTotal = len(
+            set(linha[:k+1]).intersection(set(docsRelev[i])))
+        p.append(getPrecision(releRecupTotal, k+1))
+        r.append(getRecall(releRecupTotal, len(docsRelev[i])))
+    precision.append(sum(p)/len(p))
+    recall.append(sum(r)/len(r))
+print("O tempo de busca se da por: "+str(sum(tempoBusca)/len(tempoBusca)))
+print("O tempo de Indexação é: "+str(tempoIndexacao))
+plotarGRaficos("Grafico de Recall X K ElasticSearch", y=recall,
+               x=[linha for linha in range(10)], namex="k", namey="Recall")
+plotarGRaficos("Grafico de Precision X ElasticSearch", y=precision,
+               x=[linha for linha in range(10)], namex="k", namey="Precision")
+arquivoResposta.close()
+# elif (tipo_teste == 2):
+#     tipoBusca = int(input(
+#         "Digite o tipo de busca (Linha ou Texto):\n1.Numero Estudo\n2.Conteudo\n3.Titulo\n "))
+#     if (tipoBusca == 2):
+#         busca = input("Digite o conteudo de busca: ")
+#         results = es.search(index=index_diretorio, body={
+#             "query": {"match": {"content": busca}}})
+#         flag = True
 
-# es.indices.delete(index=index_diretorio)
+#     elif (tipoBusca == 1):
+#         busca = input("Digite o numero da linha: ")
+#         results = results = results = es.search(index=index_diretorio, body={
+#             "query": {"match": {"identificador": busca}}})
+#         flag = True
+#     elif (tipoBusca == 3):
+#         busca = input("Digite o parte do Titulo do estudo: ")
+#         results = results = es.search(index=index_diretorio, body={
+#             "query": {"match": {"title": busca}}})
+#         flag = True
+#     else:
+#         flag = False
+#         print("Opção invalida")
+#     if (flag == True):
+#         for x in results['hits']['hits']:
+#             print(
+#                 f"ID:{x['_souce']['indentificador']}\nTitle: {x['_source']['title']}\nCorpo: {x['_source']['content']}\n")
+#             print('--------------------------------------------\n')
+
+es.indices.delete(index=index_diretorio)
--- Whoosh/Whosh.py
+++ Whoosh/Whosh.py
@@ -1,10 +1,29 @@
 from pathlib import Path
 from whoosh.fields import Schema, TEXT, KEYWORD, ID, STORED, NUMERIC
 from whoosh.index import create_in, open_dir
-from whoosh.qparser import QueryParser, OrGroup
+from whoosh.qparser import MultifieldParser, OrGroup
 from whoosh.scoring import BM25F
 import timeit
 import shutil
+import matplotlib.pyplot as plt
+
+
+def documentosRelevantes(nome, i):
+    val = []
+    with open(nome, "r") as arquivo:
+        for linha in arquivo:
+            if int(linha.split()[0]) == (i+1):
+                val.append(''.join(linha.split()[1]))
+
+    return val
+
+
+def getPrecision(Ra, R):
+    return Ra/R
+
+
+def getRecall(Ra, A):
+    return Ra/A
 
 
 def limparLixo(string):
@@ -15,7 +34,6 @@ def limparLixo(string):
     depoisLimpeza = timeit.default_timer()
 
     tempoLimpeza = depoisLimpeza - antesLimpeza
-
     return saida
 
 
@@ -37,17 +55,26 @@ def lerColecao(path):
 
 
 def VerificaDiretorioIndex(index_diretorio, schema):
+    tempoCriacao = 0
+    tempoAbertura = 0
     if index_diretorio.exists():
         print("Diretório de indexação já existe, abrindo...")
+        tempoAbertura = timeit.default_timer()
         index = open_dir(index_diretorio)
+        tempoAbertura = timeit.default_timer() - tempoAbertura
+        print("Tempo de abertura do diretório de indexação: " + str(tempoAbertura))
     else:
         print("Criando diretório de indexação...")
+        tempoCriacao = timeit.default_timer()
         index_diretorio.mkdir()
         index = create_in(index_diretorio, schema)
+        tempoCriacao = timeit.default_timer() - tempoCriacao
+    print("Tempo de criação do diretório de indexação: " + str(tempoCriacao))
     return index
 
 
 def lerDiretorioIndex(index, arquivo):
+    tempoIndexacao = timeit.default_timer()
     writer = index.writer()
     for linha in lerColecao(arquivo):
         content = limparLixo(linha)
@@ -56,12 +83,23 @@ def lerDiretorioIndex(index, arquivo):
         content = ''.join(''.join(content.split(" .T ")[1:]).split(" .A ")[1:])
         writer.add_document(index=i, title=title, content=content)
     writer.commit()
+    tempoIndexacao = timeit.default_timer() - tempoIndexacao
+    return tempoIndexacao
 
 
 def realizarBusca(index, searcher, busca, tipoBusca, numeroResultados):
-    query = QueryParser(tipoBusca, index.schema).parse(busca)
+    tempoBusca = timeit.default_timer()
+    query = MultifieldParser(tipoBusca, index.schema).parse(busca)
     results = searcher.search(query, limit=numeroResultados)
-    return results
+    tempoBusca = timeit.default_timer()-tempoBusca
+    return results, tempoBusca
+
+
+def plotarGRaficos(nome, x, y, namex, namey):
+    plt.plot(x, y, color='red')
+    plt.xlabel(namex)
+    plt.ylabel(namey)
+    plt.show()
 
 
 arquivo = "DataSets/cran.all.1400"
@@ -70,77 +108,107 @@ shutil.rmtree(index_diretorio)
 schema = Schema(index=TEXT(stored=True), title=TEXT(
     stored=True), content=TEXT(stored=True))
 index = VerificaDiretorioIndex(index_diretorio, schema)
-lerDiretorioIndex(index, arquivo)
-tipo_teste = int(
-    input("Digite o tipo de teste:\n1.Teste Automatico\n2.Teste Manual\n"))
-if (tipo_teste == 2):
-    searcher = index.searcher()
-    while True:
-        escolha = input("Deseja realizar uma busca? (S/N): ")
-        if (escolha == "N"):
-            break
-        elif (escolha == "S"):
-            tipoBusca = int(input(
-                "Digite o tipo de busca (Linha ou Texto):\n1.Numero Estudo\n2.Conteudo\n3.Titulo\n "))
-            if (tipoBusca == 2):
-                busca = input("Digite o conteudo de busca: ")
-                numeroResultados = int(
-                    input("Digite o número de resultados: "))
-                results = realizarBusca(index, searcher, busca,
-                                        "content", numeroResultados)
-                for result in results:
-                    content_value = result.get("content")
-                    linha_value = result.get("index")
-                    title_value = result.get("title")
-                    score = result.score
-                    print(
-                        f"Index: {linha_value}-SCORE:{score}\nTitle:{title_value}\nTexto: {content_value}")
-                    print("----------------------\n")
-            elif (tipoBusca == 1):
-                busca = input("Digite o numero da linha: ")
-                results = realizarBusca(index, searcher, busca, "index", 1)
-                for result in results:
-                    content_value = result.get("content")
-                    linha_value = result.get("index")
-                    title_value = result.get("title")
-                    score = result.score
-                    print(
-                        f"Index: {linha_value}-SCORE:{score}\nTitle:{title_value}\nTexto: {content_value}")
-                    print("----------------------\n")
-            elif (tipoBusca == 3):
-                busca = input("Digite o parte do Titulo do estudo: ")
-                results = realizarBusca(index, searcher, busca,
-                                        "title", 1)
-                for result in results:
-                    content_value = result.get("content")
-                    linha_value = result.get("index")
-                    title_value = result.get("title")
-                    score = result.score
-                    print(
-                        f"Index: {linha_value}-SCORE:{score}\nTitle:{title_value}\nTexto: {content_value}")
-                    print("----------------------\n")
-            else:
-                print("Tipo de busca inválido!")
-elif (tipo_teste == 1):
-    queryArquivo = "DataSets/cran.qry"
-    arquivoResposta = open("Respostas/respostasCran.txt", "a")
-    arquivoResposta.truncate(0)
-    searcher = index.searcher(weighting=BM25F())
-    for x, linha in enumerate(lerColecao(queryArquivo)):
+tempoIndexacao = lerDiretorioIndex(index, arquivo)
+# Caso queira Habiilitar a busca Manual descomente esse codigo e ajuste a identação:
+# tipo_teste = int(
+#     input("Digite o tipo de teste:\n1.Teste Automatico\n2.Teste Manual\n"))
+# if (tipo_teste == 2):
+#     searcher = index.searcher()
+#     while True:
+#         escolha = input("Deseja realizar uma busca? (S/N): ")
+#         if (escolha == "N"):
+#             break
+#         elif (escolha == "S"):
+#             tipoBusca = int(input(
+#                 "Digite o tipo de busca (Linha ou Texto):\n1.Numero Estudo\n2.Conteudo\n3.Titulo\n "))
+#             if (tipoBusca == 2):
+#                 busca = input("Digite o conteudo de busca: ")
+#                 numeroResultados = int(
+#                     input("Digite o número de resultados: "))
+#                 results = realizarBusca(index, searcher, busca,
+#                                         ["content"], numeroResultados)
+#                 for result in results:
+#                     content_value = result.get("content")
+#                     linha_value = result.get("index")
+#                     title_value = result.get("title")
+#                     score = result.score
+#                     print(
+#                         f"Index: {linha_value}-SCORE:{score}\nTitle:{title_value}\nTexto: {content_value}")
+#                     print("----------------------\n")
+#             elif (tipoBusca == 1):
+#                 busca = input("Digite o numero da linha: ")
+#                 results = realizarBusca(index, searcher, busca, ["index"], 1)
+#                 for result in results:
+#                     content_value = result.get("content")
+#                     linha_value = result.get("index")
+#                     title_value = result.get("title")
+#                     score = result.score
+#                     print(
+#                         f"Index: {linha_value}-SCORE:{score}\nTitle:{title_value}\nTexto: {content_value}")
+#                     print("----------------------\n")
+#             elif (tipoBusca == 3):
+#                 busca = input("Digite o parte do Titulo do estudo: ")
+#                 results = realizarBusca(index, searcher, busca,
+#                                         ["title"], 1)
+#                 for result in results:
+#                     content_value = result.get("content")
+#                     linha_value = result.get("index")
+#                     title_value = result.get("title")
+#                     score = result.score
+#                     print(
+#                         f"Index: {linha_value}-SCORE:{score}\nTitle:{title_value}\nTexto: {content_value}")
+#                     print("----------------------\n")
+#             else:
+#                 print("Tipo de busca inválido!")
+# elif (tipo_teste == 1):
+queryArquivo = "DataSets/cran.qry"
+arquivoResposta = open("Respostas/respostasCran.txt", "a")
+arquivoResposta.truncate(0)
+searcher = index.searcher(weighting=BM25F())
+precision = []
+recall = []
+tempoBusca = []
+perguntas = []
+docsRelev = []
+docs = []
+for x, linha in enumerate(lerColecao(queryArquivo)):
+    doc = []
+    perguntas.append(x+1)
+    busca = limparQuery(linha)
+    i = ''.join(busca.split(" .W ")[:1])
+    busca = ''.join(busca.split(" .W ")[1:])
+    buscarOR = busca.replace(" ", " OR ")
+    results, temp = realizarBusca(
+        index, searcher, buscarOR, ["content", "title"], 10)
+    for (result) in results:
+        doc.append(result.get('index'))
+        arquivoResposta.write(
+            f"Linha={x+1}-ID Questao:{i}- ID Documento: {result.get('index')}-Score: {result.score}\n")
+        arquivoResposta.write(f"Pergunta: {busca}\n")
+        arquivoResposta.write(f"Resposta: {result.get('title')}\n")
+        arquivoResposta.write(
+            "-------------------------------------------------\n")
+    docReelev = documentosRelevantes("DataSets/cranqrel", x)
+    docsRelev.append(docReelev)
+    docs.append(doc)
+    tempoBusca.append(temp)
+for k in range(10):
+    p = []
+    r = []
+    for i, linha in enumerate(docs):
 
-        busca = limparQuery(linha)
-        i = ''.join(busca.split(" .W ")[:1])
-        busca = ''.join(busca.split(" .W ")[1:])
-        buscarOR = busca.replace(" ", " OR ")
-        results = realizarBusca(
-            index, searcher, buscarOR, "content", 10)
-        for (result) in results:
-            arquivoResposta.write(
-                f"Linha={x+1}-ID Questao:{i}- ID Documento: {result.get('index')}-Score: {result.score}\n")
-            arquivoResposta.write(f"Pergunta: {busca}\n")
-            arquivoResposta.write(f"Resposta: {result.get('title')}\n")
-            arquivoResposta.write(
-                "-------------------------------------------------\n")
+        releRecupTotal = len(
+            set(linha[:k+1]).intersection(set(docsRelev[i])))
+        p.append(getPrecision(releRecupTotal, k+1))
+        r.append(getRecall(releRecupTotal, len(docsRelev[i])))
+    precision.append(sum(p)/len(p))
+    recall.append(sum(r)/len(r))
+print("O tempo de busca se da por: "+str(sum(tempoBusca)/len(tempoBusca)))
+print("O tempo de Indexação é: "+str(tempoIndexacao))
+plotarGRaficos("Grafico de Recall X K Whoosh", y=recall,
+               x=[linha for linha in range(10)], namex="k", namey="Recall")
+plotarGRaficos("Grafico de Precision X K Whoosh", y=precision,
+               x=[linha for linha in range(10)], namex="k", namey="Precision")
 
-    arquivoResposta.close()
+arquivoResposta.close()
 index.close()
